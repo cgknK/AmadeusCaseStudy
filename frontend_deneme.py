56626e1503a8bd97b11e9a4ee493316484d5d90c
cKey <klvzcgkn@gmail.com>
2023-09-03 15:00:20 +0300

    add frontend_deneme.py for selenyum

---
+++ frontend_deneme.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service('./chromedriver-win64/chromedriver.exe')
+driver =webdriver.Chrome(service=service)
+
+driver.get('http://www.apple.com')
