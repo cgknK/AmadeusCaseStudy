e34943a1ceb074e3311a72bccd862e77fd058d76
cKey <klvzcgkn@gmail.com>
2023-09-05 12:07:17 +0300

    pytest'li olana Failleri ss alma eklendi

--- frontend_tests_pytest.py
+++ frontend_tests_pytest.py
@@ -25,44 +25,70 @@ class TestFrontendCases:
 
         if not os.path.exists("./res"):
             os.mkdir("./res")
+        """
         else:
             for file in os.listdir("./res"):
                 file_path = os.path.join("./res", file)
                 os.remove(file_path)
         #print("setup_method.")
-
+        """
         yield self.driver
 
         self.driver.quit()
 
+
     @pytest.fixture(scope="function")
     def teardown_method(self, request):
-        #error = sys.exc_info()
+        """
         print("s._t", request.node.name)
         print("s._t2", request.node.originalname )
         print("sys[0]", sys.exc_info()[0])
         print("sys[1]", sys.exc_info()[1])
         print("sys[2]", sys.exc_info()[2])
+        print("request.node.outcome", request.node.outcome)
         if sys.exc_info()[0]:
             test_method_name = request.node.name
             self.driver.save_screenshot(f"./res/{test_method_name}.png")
         #self.driver.quit()
-        self.driver.refresh()
+        """
+        try:
+            print("yield+")
+            yield
+            print("yield-")
+        except:
+            test_method_name = request.node.name
+            self.driver.save_screenshot(f"./res/{test_method_name}.png")
+            print(f"ss: ./res/{test_method_name}.png")
+        finally:
+            self.driver.refresh()
+            print("teardown_method f5")
+            #print("Test fonksiyonunun sonucu:", request.node.outcome)
+        
 
-
-    def test_not_same_value(self, driver_setup, teardown_method):
+    #@pytest.mark.parametrize("a,b", [(1, 2), (2, 3), (3, 4)])
+    def test_not_same_value(self, request, driver_setup, teardown_method):
         #print("test_not_same_value+")
         #self.driver.refresh()
         for i in self.cities:
             fca.write_from_side(self.driver, i)
             fca.write_to_side(self.driver, i)
-            assert fca.read_from_side(self.driver) != fca.read_to_side(self.driver)
+            try:
+                print("assert+")
+                assert fca.read_from_side(self.driver) != fca.read_to_side(self.driver)
+                print("assert-")
+            except:
+                test_method_name = request.node.name
+                self.driver.save_screenshot(f"./res/{test_method_name}_{i}.png")
+                print(f"ss: ./res/{test_method_name}.png")
+            finally:
+                print("ff")
+                #request.node.outcome
         #ResourceWarning self.driver.exit()/quit()
         #self.driver.quit()
         #print("test_not_same_value.")
 
-
-    def test_same_x(self, driver_setup, teardown_method):
+    #ss and parametrize
+    def test_asame_x(self, driver_setup, teardown_method):
         #print("test_same_x.")
         #self.driver.refresh()
         for i in self.cities[:3]:
